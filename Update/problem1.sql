712fef6a6c75a7014f5fe86f5cc4156c316671d6
Dylan Rigney <dylantrigney@gmail.com>
2023-01-12 22:17:37 +0000

    Saved progress on SelectWhereAndOr 2023-01-12 22:17:37 +0000

---
+++ Update/problem1.sql
@@ -0,0 +1,11 @@
+--UPDATE site_user SET lastname = 'Rush' WHERE firstname = 'Alexa';
+
+UPDATE site_user
+SET lastname = 'Rush'
+WHERE id = 2; 
+
+
+
+
+--UPDATE table_name SET col_1 = val_1, col_2 = val_2, ...col_N = val_N WHERE condition;
+--Update Alexa's last name to be 'Rush' in the site_user table.
